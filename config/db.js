ae8ba4a91f0ff2caeb1860937bf80a4cf94e9c8a
Aniketikhar <aniketikhar88@gmail.com>
2026-06-22 10:30:30 +0530

    removing consoles

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    console.log(`MongoDB: ${process.env.MONGO_URI}`);
+    // console.log(`MongoDB: ${process.env.MONGO_URI}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    console.log(`MongoDB: ${process.env.MONGO_URI}`);
+    // console.log(`MongoDB: ${process.env.MONGO_URI}`);
     console.warn("Warning: Continuing server execution without MongoDB connection.");
   }
 };
